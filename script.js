e5c844f7b8c8f0b7d6cd7e3e1b95a271919d370b
Abir Bouhriz Daidj <64310997+Abir-Bouhriz@users.noreply.github.com>
2020-06-05 21:59:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItem = document.querySelectorAll('.nav-item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+	if (!showMenu) {
+		menuBtn.classList.add('close');
+		menu.classList.add('show');
+		menuNav.classList.add('show');
+		menuBranding.classList.add('show');
+		navItem.forEach(item => item.classList.add('show'));
+
+showMenu = true;
+	} 
+
+	else{
+		menuBtn.classList.remove('close');
+		menu.classList.remove('show');
+		menuNav.classList.remove('show');
+		menuBranding.classList.remove('show');
+		navItem.forEach(item => item.classList.remove('show'));
+
+showMenu = false;
+	}
+}
+
